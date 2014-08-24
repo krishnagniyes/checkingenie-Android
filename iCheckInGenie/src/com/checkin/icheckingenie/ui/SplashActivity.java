dbdf7b112b38ecb1d9b473a979ce6e70a7608c2e
Krishna <krishnakumar@Infogains-MacBook-Pro-8.local>
2014-08-24 14:16:04 -0700

    added splash

--- iCheckInGenie/src/com/checkin/icheckingenie/ui/SplashActivity.java
+++ iCheckInGenie/src/com/checkin/icheckingenie/ui/SplashActivity.java
@@ -1,19 +1,39 @@
 package com.checkin.icheckingenie.ui;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class SplashActivity extends ActionBarActivity {
+	 /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+//                Intent mainIntent = new Intent(SplashActivity.this,Menu.class);
+//                SplashActivity.this.startActivity(mainIntent);
+//                SplashActivity.this.finish();
+            	
+            	// Launch the next activity...
+            	String url = "http://www.google.com";
+            	Intent i = new Intent(Intent.ACTION_VIEW);
+            	i.setData(Uri.parse(url));
+            	startActivity(i);
+            	 SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
